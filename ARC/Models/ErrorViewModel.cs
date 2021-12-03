4ac83914dfa3f474b7e80f928a5ef713bf71da88
Ziaur Rab <rabziaur77@outlook.com>
2021-12-04 02:50:45 +0530

    Add project files.

---
+++ ARC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ARC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
